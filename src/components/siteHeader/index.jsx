0e9436bf9d82a6d5c2514beb57cbdb04ad6fecdd
hleab <hleabrszylx@gmail.com>
2025-10-21 12:10:57 +0100

    Added Trending This Week page.

--- src/components/siteHeader/index.jsx
+++ src/components/siteHeader/index.jsx
@@ -26,7 +26,7 @@ const SiteHeader = () => {
   const menuOptions = [
     { label: "Home", path: "/" },
     { label: "Favorites", path: "/movies/favorites" },
-    { label: "Option 3", path: "/" },
+    { label: "TRENDING THIS WEEK", path: "/movies/trending/this-week" },
     { label: "Option 4", path: "/" },
   ];
 
